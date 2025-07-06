5bcf1f986bdfbf1bd373c2e760d0c8ec2d61afec
akd <aliaksei.krauchanka.dev@gmail.com>
2025-07-06 21:48:39 +0200

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <UserButton />
           </SignedIn>
           {children}
-          {children}
         </body>
       </html>
     </ClerkProvider>
