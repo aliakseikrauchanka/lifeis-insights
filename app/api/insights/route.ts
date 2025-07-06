7c7daccc2dbbe0ead602e49458413f1aac797ee2
akd <aliaksei.krauchanka.dev@gmail.com>
2025-07-06 21:32:39 +0200

    fix name

--- app/api/insights/route.ts
+++ app/api/insights/route.ts
@@ -3,7 +3,7 @@ import { Client } from '@notionhq/client';
 import { NextRequest, NextResponse } from 'next/server';
 import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const notion = new Client({ auth: process.env.NEXT_NOTION_API_KEY });
 const insightsDatabaseId = 'caae7cadc59c43fe920dd5ce4048dade';
 
 export async function GET(req: NextRequest) {
